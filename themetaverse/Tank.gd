20a03688dd9767710e3872078f6f90a04079d83c
jade <jadehiggins2000@gmail.com>
2023-10-14 19:53:57 +0100

    getting somewhere

--- themetaverse/Tank.gd
+++ themetaverse/Tank.gd
@@ -1,60 +1,14 @@
 extends CharacterBody3D
 
-@export var speed = 10.0
-@export var rotSpeed = 5.0
-@export var fireRate = 5;
+@export var move_speed:float=5
+@export var rot_speed:float=1
 
-@export var bulletPrefab:PackedScene
-
-@export var can_fire = true; 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-func _drawGizmos():
-
-	pass
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	$Timer.connect("timeout", Callable(self, "enableFire"))
-		
-	pass # Replace with function body.
+func _process(delta):	
+	var move = Input.get_axis("ui_down", "ui_up")
+	print(move)
 	
-func enableFire():
-	can_fire = true;
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):		
-	# rotate_y(0.1)
-	var turn = Input.get_axis("turn_left", "turn_right")
+	translate(Vector3.FORWARD * move * delta * move_speed)
 	
-	if abs(turn) > 0:     
-		# rotate()
-		rotation.y -= rotSpeed * delta * turn
+	var turn = Input.get_axis("ui_left", "ui_right")
 	
-	# rotate_y(0.1)
-	# rotate_x(0.1)
-	var move = Input.get_axis("move_for", "move_back")
-	if abs(move) > 0:     
-		# set_velocity(- transform.basis.z * speed * move)
-		# move_and_slide()
-		translate(Vector3.FORWARD * speed * move * delta)
-		# move_and_collide()
-		
-		
-	if can_fire and Input.is_action_pressed("ui_select"):
-		var bullet = bulletPrefab.instantiate()
-		$"..".add_child(bullet)
-		bullet.rotation = rotation
-		bullet.global_transform.origin = $CharacterBody3D/Turret/bulletSpawn.global_transform.origin				
-		can_fire = false
-		$Timer.start(1.0 / fireRate)
-
-		
-	_drawGizmos()
-	
-	
-	
-	
+	rotate(Vector3.UP, turn * rot_speed * delta)
